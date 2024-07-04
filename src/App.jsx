0280d424f90f49c5833e7484ec66bee784c7a4f3
kayodeolowo <officialkayodeolowo@gmail.com>
2024-07-04 13:19:54 +0100

    flow bite setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from "flowbite-react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-red-400  font-bold text-4xl'> helklo boy </h1>
+      <Button>Click me</Button>;
+    </div>
+  )
+}
+
+export default App
